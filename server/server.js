f8f217cac2d665cdb47dd26f7694bb0889002156
Aleksandr Kozlov <kaa_12@mail.ru>
2020-05-14 15:07:06 +0400

    Add react

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const randomString = require('./randomString');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render = () => {
+    return (
+      <main>
+        Hello React
+      </main>
+    );
+  }
+}
+
+export default App;
